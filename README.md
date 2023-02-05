a94c56da61ff8d98594b4df72248a3bbf80bdbff
Adelina Gorodetskaya <delyunchik@gmail.com>
2023-02-05 23:19:22 +0300

    results printing

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 <B>Author:</B><BR>
 Adelina Gorodetskaya @delyunchik
+
+quiz_id - ID теста из библиотеки тестов, quiz_id=0 встроенный демо тест на тригонометрические функции
+test_id - уникальный ID (uuid) экземпляра теста с quiz_id
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import config
 import logging
+import aioschedule
+import asyncio
+from uuid import uuid4
 from datetime import datetime, timedelta
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
@@ -28,25 +31,94 @@ bot = Bot(token=config.API_TOKEN, proxy=config.PROXY_URL)
 # за хэндлеры отвечает специальный Диспетчер
 dp = Dispatcher(bot)
 
+tests = {}  # экземпляры проведенных тестов, состоят из polls
+polls = {}  # отдельные вопросы
+
+
+class Test:
+    close_dt: datetime
+    quiz_id: str
+    poll_ids: list() = []  # идентификаторы poll_id
+    user_id: int
+    chat_id: int
+    users: dict() = {}
+
+
+class Poll:
+    test_id: int
+    correct_option_id: int
+
+    def __init__(self, test_id: int, correct_option_id: int) -> None:
+        self.test_id = test_id
+        self.correct_option_id = correct_option_id
+
+
+class User:
+    full_name: str
+    correct_answers: int = 0
+
+    def __init__(self, full_name: str) -> None:
+        self.full_name = full_name
+
+
+async def print_results(test_id, chat_id):
+    # logging.debug('schedule userid={}'.format(userid))
+    text = bold('Результаты:\n')
+    users = tests[test_id].users
+    num_polls = len(tests[test_id].poll_ids)
+    for user in users:
+        text += '{} @{} {}/{}'.format(
+            users[user].full_name,
+            user,
+            users[user].correct_answers,
+            num_polls
+        )
+    await bot.send_message(
+        chat_id=chat_id,
+        text=text,
+        parse_mode=ParseMode.MARKDOWN,
+    )
+    return aioschedule.CancelJob
+
 
 @dp.message_handler(commands=['quiz'])
 async def command_quiz(message: types.Message):
-    # logging.debug('QUIZ request userid={}'.format(userid))
-    n = (len(quiz)-1) // 4  # число вопросов
-    close_dt = datetime.now() + timedelta(minutes=int(quiz[0]))
+    quiz_id = 0  # id теста из библиотеки тестов
+    logging.info('QUIZ command msg={}'.format(message.as_json()))
+    k = 2  # число общих параметров
+    n = (len(quiz)-k) // 4  # число вопросов
+    test_id = uuid4()  # уникальный ID экземпляра теста
+    close_dt = datetime.now() + timedelta(seconds=int(quiz[1]))
+    test = Test()
+    test.close_dt = close_dt
+    test.quiz_id = quiz_id
+    test.user_id = message.from_id
+    test.chat_id = message.chat.id
+    tests[test_id] = test
+    msg = await bot.send_message(
+        chat_id=message.chat.id,
+        text=text(
+            bold(quiz[0]),
+            'Время окончания теста: {}'.format(
+                close_dt.strftime('%X')),
+            sep='\n'
+        ),
+        parse_mode=ParseMode.MARKDOWN,
+    )
+    # отправим вопросы
     for i in range(n):
         msg = await bot.send_message(
             chat_id=message.chat.id,
-            text='[{}/{}] {}'.format(i+1, n, quiz[i*4+1]),
+            text=r'[{}/{}] {}'.format(i+1, n, quiz[i*4+k]),
         )
         msg = await bot.send_photo(
             chat_id=message.chat.id,
-            photo=formula(quiz[i*4+2]),
+            photo=formula(quiz[i*4+k+1]),
         )
-        q = quiz[i*4+4].count('\n')+1  # число ответов
+        q = quiz[i*4+k+3].count('\n')+1  # число ответов
         msg = await bot.send_photo(
             chat_id=message.chat.id,
-            photo=formula(quiz[i*4+4]),
+            photo=formula(quiz[i*4+k+3]),
         )
         msg = await bot.send_poll(
             chat_id=message.chat.id,
@@ -56,9 +128,18 @@ async def command_quiz(message: types.Message):
             type='quiz',
             close_date=close_dt,
             protect_content=True,
-            correct_option_id=quiz[i*4+3]-1,
+            correct_option_id=quiz[i*4+k+2]-1,
         )
-    logging.info('i={} msg={}'.format(i, msg.as_json()))
+        polls[msg.poll.id] = Poll(test_id, msg.poll.correct_option_id)
+        tests[test_id].poll_ids.append(msg.poll.id)
+        logging.info('i={} msg={}'.format(i, msg.as_json()))
+    # заведем таймер на окончание теста для вывода результатов
+    results_dt = close_dt + timedelta(minutes=1)
+    tm = results_dt.strftime('%H:%M')
+    aioschedule.every().day.at(tm).do(
+        print_results,
+        test_id=test_id,
+        chat_id=message.chat.id)
 
 
 @dp.poll_answer_handler()
@@ -67,13 +148,17 @@ async def handle_poll_answer(quiz_answer: types.PollAnswer):
     Это хендлер на новые ответы в опросах (Poll) и викторинах (Quiz)
     Реагирует на изменение голоса. В случае отзыва голоса тоже срабатывает!
 
-    Чтобы не было путаницы:
-    * quiz_answer - ответ на активную викторину
-    * saved_quiz - викторина, находящаяся в нашем "хранилище" в памяти
-
     :param quiz_answer: объект PollAnswer с информацией о голосующем
     """
     logging.info(quiz_answer.as_json())
+    username = quiz_answer.user.username
+    poll_id = quiz_answer.poll_id
+    test_id = polls[poll_id].test_id
+    users = tests[test_id].users
+    if username not in users:
+        users[username] = User(quiz_answer.user.full_name)
+    if quiz_answer.option_ids[0] == polls[poll_id].correct_option_id:
+        users[username].correct_answers += 1
 
 
 # обработчик команды start
@@ -88,6 +173,7 @@ async def command_start(message: types.Message):
 # обработчик команды help
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
+    logging.info('HELP command msg={}'.format(message.as_json()))
     # сформируем текст сообщения
     msg = text(
         'Создать викторину /quiz',
@@ -110,9 +196,18 @@ async def unknown_message(msg: types.Message):
     await msg.reply(message_text, parse_mode=ParseMode.MARKDOWN)
 
 
+# Обработчик заданий по расписанию
+async def scheduler():
+    logging.info('Старт Scheduler()')
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(5)
+
+
 # Обработчик начала работы бота
 async def startup(_):
     logging.info('Старт работы бота!')
+    asyncio.create_task(scheduler())
 
 
 # Обработчик завершения работы
@@ -126,4 +221,5 @@ if __name__ == '__main__':
     executor.start_polling(
         dp, on_startup=startup,
         on_shutdown=shutdown,
-        skip_updates=True)
+        skip_updates=True)  # не обрабатывать сообщения
+                            # присланные при остановленном боте
